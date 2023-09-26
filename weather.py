6a5afa038f055276af3a0fc9ed3eada41f974acc
nmslking <justkobe@gmail.com>
2023-09-27 02:42:46 +0800

    first commit

---
+++ weather.py
@@ -0,0 +1,52 @@
+# import requests
+# from dotenv import load_dotenv
+# import os
+# from pprint import pprint
+
+# load_dotenv()
+
+# def get_current_weather():
+#     print('\n*** Get Current Weather Conditions ***\n')
+
+#     city = input("\nPlease enter a city name:\n")
+
+#     request_url = f'https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=metric'
+
+#     # print(request_url)
+#     weather_data = requests.get(request_url).json()
+
+#     #pprint(weather_data)
+#     print(f"\nCurrent weather for {weather_data['name']}")
+#     print(f"\nThe temp is {weather_data['main']['temp']}")
+#     print(f"\nFeels like {weather_data['main']['feels_like']} and {weather_data['weather'][0]['description'].title()}.")
+
+# if __name__ == "__main__":
+#     get_current_weather()
+
+
+import requests
+from dotenv import load_dotenv
+import os
+from pprint import pprint
+
+load_dotenv()
+
+def get_current_weather(city="dongguan"):
+
+
+    request_url = f'https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=metric'
+
+
+    weather_data = requests.get(request_url).json()
+    return weather_data
+
+if __name__ == "__main__":
+    print('\n*** Get Current Weather Conditions ***\n')
+    city = input("\nPlease enter a city name:\n")
+    # Check for empty strings or string with only spaces
+    if not bool(city.strip()):
+        city = "Dongguan"
+    weather_data = get_current_weather(city)
+    print("\n")
+    pprint(weather_data)
+
